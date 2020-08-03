c284558663b8505f361052c81f628dfe5f0efef5
YONG JI <braveji18@gmail.com>
2020-08-03 15:58:26 +0900

    JB금융지주

--- 02_cdsw/04_jbfg/cdsw1.7/Python/save_docker.sh
+++ 02_cdsw/04_jbfg/cdsw1.7/Python/save_docker.sh
@@ -20,23 +20,12 @@ fi
 CURRENT_TIME='date +"%H:%M:%S"'
 echo "####start  sh save_docker.sh     `$CURRENT_TIME` ####"
 
-#docker save  cuda9.docker.repository.cloudera.com/cdsw/engine:8  | gzip > cuda9.docker.repository.cloudera.com.tar.gz 
-#docker save  cuda10.docker.repository.cloudera.com/cdsw/engine:8  | gzip > cuda10.docker.repository.cloudera.com.tar.gz 
+docker save  conda.docker.repository.cloudera.com/cdsw/engine:10  | gzip > conda.docker.repository.cloudera.com.tar.gz 
 
-#docker save  conda.cuda9.docker.repository.cloudera.com/cdsw/engine:8  | gzip > conda.cuda9.docker.repository.cloudera.com.tar.gz 
-#docker save  conda.cuda10.docker.repository.cloudera.com/cdsw/engine:8  | gzip > conda.cuda10.repository.cloudera.com.tar.gz 
+docker save  tensorflow2.3.${SITE_DOMAIN}/cdsw/engine:10.${TODAY}  | gzip > tensorflow2.3.${SITE_DOMAIN}_${TODAY}.tar.gz 
 
-docker save  tensorflow2.0.${SITE_DOMAIN}/cdsw/engine:8.${TODAY}  | gzip > tensorflow2.0.${SITE_DOMAIN}_${TODAY}.tar.gz 
+docker save  tensorflow1.15.${SITE_DOMAIN}/cdsw/engine:10.${TODAY} | gzip > tensorflow1.15.${SITE_DOMAIN}_${TODAY}.tar.gz 
 
-docker save  tensorflow1.12.${SITE_DOMAIN}/cdsw/engine:8.${TODAY} | gzip > tensorflow1.12.${SITE_DOMAIN}_${TODAY}.tar.gz 
-docker save  tensorflow1.12.NoneGPU.${SITE_DOMAIN}/cdsw/engine:8.${TODAY} | gzip > tensorflow1.12.NoneGpu.${SITE_DOMAIN}_${TODAY}.tar.gz 
-
-docker save  tensorflow1.15.${SITE_DOMAIN}/cdsw/engine:8.${TODAY} | gzip > tensorflow1.15.${SITE_DOMAIN}_${TODAY}.tar.gz 
-
-docker save  pytorch1.3.${SITE_DOMAIN}/cdsw/engine:8.${TODAY}  | gzip >  pytorch1.3.${SITE_DOMAIN}_${TODAY}.tar.gz
-
-#docker save  mobilenet.${SITE_DOMAIN}/cdsw/engine:8.${TODAY}  | gzip >  mobilenet.${SITE_DOMAIN}_${TODAY}.tar.gz
-
-docker save  tensorflow1.12.multiGPU.${SITE_DOMAIN}/cdsw/engine:8.${TODAY} | gzip > tensorflow1.12.multiGpu.${SITE_DOMAIN}_${TODAY}.tar.gz 
+docker save  pytorch1.3.${SITE_DOMAIN}/cdsw/engine:10.${TODAY}  | gzip >  pytorch1.3.${SITE_DOMAIN}_${TODAY}.tar.gz 
 
 echo "####end  sh save_docker.sh     `$CURRENT_TIME` ####"
